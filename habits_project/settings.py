b07fdb4c6614be5ff2189b86e048e1e13e5b0c4d
Godemd <dandigrom1@gmail.com>
2024-12-28 03:14:38 +0300

    Fix

--- habits_project/settings.py
+++ habits_project/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Redis настройки
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
