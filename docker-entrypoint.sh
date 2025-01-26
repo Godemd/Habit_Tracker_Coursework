216876cdb0984bf03a9026f500d056dfa1eb72a3
Godemd <dandigrom1@gmail.com>
2025-01-26 13:17:31 +0300

    Обновление Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-python manage.py migrate
-python manage.py collectstatic --no-input
-gunicorn habits_project.wsgi:application --bind 0.0.0.0:8000 
+# Выход при ошибке любой команды
+set -e
+
+# Применяем миграции
+echo "Applying migrations..."
+poetry run python manage.py migrate
+
+# Сбор статических файлов
+echo "Collecting static files..."
+poetry run python manage.py collectstatic --noinput
